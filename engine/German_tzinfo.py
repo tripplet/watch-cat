9a171b164a64eb2d602e7479a25bfbf83e5943b8
Tobias Tangemann <tobiastangemann@gmail.com>
2013-10-17 10:59:39 +0200

    Fix timezone shit

--- engine/German_tzinfo.py
+++ engine/German_tzinfo.py
@@ -4,23 +4,26 @@ class German_tzinfo(tzinfo):
   """Implementation of the German timezone."""
 
   def utcToLocal(self, dt):
-    return dt + self.utcoffset(dt);
+    return dt + self.utcoffset(dt)
 
   def utcoffset(self, dt):
     return timedelta(hours=+1) + self.dst(dt)
 
   def _LastSunday(self, dt):
-    """First Sunday on or after dt."""
-    return dt + timedelta(days=(dt.weekday()-6))
+    """Previous Sunday on or bevor dt."""
+    if dt.isoweekday() == 7:
+      return dt
+    else:
+      return dt + timedelta(days=-dt.isoweekday())
 
   def dst(self, dt):
-    # 2 am on the last Sunday in March
+    # 2 am (02:00) on the last Sunday in March
     dst_start = self._LastSunday(datetime(dt.year, 3, 31, 2))
 
-    # 3 am on the last Sunday in October
-    dst_end = self._LastSunday(datetime(dt.year, 10, 1, 3))
+    # 3 am (03:00) on the last Sunday in October
+    dst_end = self._LastSunday(datetime(dt.year, 10, 31, 3))
 
     if dst_start <= dt.replace(tzinfo=None) < dst_end:
-      return timedelta(hours=1)
+      return timedelta(hours=+1)
     else:
       return timedelta(hours=0)
