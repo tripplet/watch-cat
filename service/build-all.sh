8b49d4bd5493cde300bda14caf0d3746ca073fc6
Tobias Tangemann <tobias@tangemann.org>
2017-08-28 20:08:34 +0200

    Read arguments from environment

--- service/build-all.sh
+++ service/build-all.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-env GOOS=linux GOARCH=arm go build -o watch-cat-arm
-env GOOS=linux GOARCH=amd64 go build -o watch-cat-linux
-env GOOS=darwin GOARCH=amd64 go build -o watch-cat-macos
-env GOOS=windows GOARCH=amd64 go build -o watch-cat-win
+mkdir bin
+env GOOS=linux GOARCH=arm go build -o bin/watchcat-arm
+env GOOS=linux GOARCH=amd64 go build -o bin/watchcat-linux
+env GOOS=darwin GOARCH=amd64 go build -o bin/watchcat-macos
+env GOOS=windows GOARCH=amd64 go build -o bin/watchcat-win
--- service/main.go
+++ service/main.go
@@ -4,33 +4,52 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 	"runtime"
+	"strings"
 	"time"
 )
 
+var param = map[string]*string{
+	"repeat": nil,
+	"url":    nil,
+	"key":    nil,
+}
+
 func main() {
-	delayStr := flag.String("repeat", "0", "Repeat request after interval")
-	url := flag.String("url", "", "Url where to send requests")
-	key := flag.String("key", "", "Secret key to use")
+	param["repeat"] = flag.String("repeat", "0", "Repeat request after interval")
+	param["url"] = flag.String("url", "", "Url where to send requests")
+	param["key"] = flag.String("key", "", "Secret key to use")
 	flag.Parse()
 
-	delay, err := time.ParseDuration(*delayStr)
+	// Environment variables can override cmdline parameter
+	for p := range param {
+		if os.Getenv(strings.ToUpper(p)) != "" {
+			*param[p] = os.Getenv(strings.ToUpper(p))
+		}
+	}
+
+	delay, err := time.ParseDuration(*param["repeat"])
 	if err != nil {
 		panic(err)
 	}
 
-	sendRequest(*url, *key)
+	// Imidiadly send first request
+	sendRequest(*param["url"], *param["key"])
 
+	// Do not repeat
 	if delay == 0 {
 		return
 	}
 
+	// Repeat request forever
 	for _ = range time.Tick(delay) {
-		sendRequest(*url, *key)
+		sendRequest(*param["url"], *param["key"])
 		runtime.GC()
 	}
 }
 
 func sendRequest(url string, key string) {
+	fmt.Printf("%s?key=%s&uptime=%d\n", url, key, GetUptime())
 	http.Get(fmt.Sprintf("%s?key=%s&uptime=%d", url, key, GetUptime()))
 }
