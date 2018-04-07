e941bdf02ed6d276d1b444b3ad2870e80251f7fb
Tobias Tangemann <tobias@tangemann.org>
2018-04-07 20:06:48 +0200

    Remove debug symbols for release binaries

--- service/build-all.sh
+++ service/build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir bin
-env GOOS=linux GOARCH=arm go build -o bin/watchcat-arm
-env GOOS=linux GOARCH=amd64 go build -o bin/watchcat-linux
-env GOOS=darwin GOARCH=amd64 go build -o bin/watchcat-macos
-env GOOS=windows GOARCH=amd64 go build -o bin/watchcat-win
+env GOOS=linux GOARCH=arm go build -ldflags "-s -w" -o bin/watchcat-arm
+env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o bin/watchcat-linux
+env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o bin/watchcat-macos
+env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o bin/watchcat-win
