fc5b019113061d5c68fadd7ff03cddb9fa21311b
Tobias Tangemann <tobias@tangemann.org>
2021-03-07 22:23:17 +0100

    Improve build

--- service/golang/build-all.sh
+++ service/golang/build-all.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-mkdir bin
-env GOOS=linux GOARCH=arm go build -ldflags "-s -w" -o bin/watchcat-arm
-env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o bin/watchcat-linux
-env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o bin/watchcat-macos
-env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o bin/watchcat-win
+
+mkdir -p bin
+env GOOS=linux GOARCH=arm go build -ldflags "-s -w" -trimpath -o bin/watchcat-arm
+env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -trimpath -o bin/watchcat-linux
+env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -trimpath -o bin/watchcat-macos
+env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -trimpath -o bin/watchcat-win
